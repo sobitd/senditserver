0b842a54e9d3d7cd5af5d6cc0eaf3eb0c5e2e789
sobitd <sobitdaniel@gmail.com>
2022-10-31 21:50:07 +0300

    create apikeys model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :parcels
   validates :first_name, :last_name, :email_address, :phone_number, :username, :password_digest, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_182320) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_184642) do
+  create_table "api_keys", force: :cascade do |t|
+    t.integer "bearer_id", null: false
+    t.string "bearer_type", null: false
+    t.string "token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bearer_id", "bearer_type"], name: "index_api_keys_on_bearer_id_and_bearer_type"
+    t.index ["token"], name: "index_api_keys_on_token", unique: true
+  end
+
   create_table "parcels", force: :cascade do |t|
     t.string "recipient_name"
     t.string "recipient_address"
