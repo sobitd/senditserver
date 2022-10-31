48cc4eb816b042ef46e62342f254a6c24ec30463
sobitd <sobitdaniel@gmail.com>
2022-10-31 21:22:16 +0300

    add validations

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -1,2 +1,4 @@
 class Parcel < ApplicationRecord
+  validates :recipient_name, :recipient_address, :recipient_contact, :weight, :from, :destination, presence: true
+  validates_inclusion_of :order_status, in: %w[delivered processed in-transit]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
+    validates :first_name, :last_name, :email_address, :phone_number, :username, :password_digest, presence: true
 end
