f51624166398987836bd80540fb065e648a9c12d
sobitd <sobitdaniel@gmail.com>
2022-10-31 21:24:09 +0300

    add foreign key to parcels

---
+++ db/migrate/20221031182320_add_foreign_key_to_parcel.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToParcel < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :parcels, :user, null: false, foreign_key: true
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_182320) do
+  create_table "parcels", force: :cascade do |t|
+    t.string "recipient_name"
+    t.string "recipient_address"
+    t.string "recipient_contact"
+    t.integer "weight"
+    t.string "from"
+    t.string "destination"
+    t.integer "total_cost"
+    t.string "order_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_parcels_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "username"
+    t.string "password_digest"
+    t.string "email_address"
+    t.boolean "isAdmin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "parcels", "users"
+end
